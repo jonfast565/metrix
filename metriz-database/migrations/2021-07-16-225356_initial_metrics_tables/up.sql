4c2b642ddbe29bd48c165f0b1aa131ad1e177c2d
Jon Fast <jnfstdj656@gmail.com>
2021-07-21 01:44:37 -0400

    database scripts

--- metriz-database/migrations/2021-07-16-225356_initial_metrics_tables/up.sql
+++ metriz-database/migrations/2021-07-16-225356_initial_metrics_tables/up.sql
@@ -1,1 +1,14 @@
 -- Your SQL goes here
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE metric (
+    id uuid primary key not null default uuid_generate_v4(),
+    data_point varchar(255) not null,
+    data_type varchar(255) not null,
+    data_value_numeric decimal(50, 50) not null,
+    created_by varchar(255) not null default session_user,
+    created_datetime datetime not null default now(),
+    last_modified_by varchar(255) not null default session_user,
+    last_modified_by_datetime datetime not null default now()
+);
