ccb466bdcfc4a5c4a3f7583615ce9ba4228a455d
Jon Fast <jnfstdj656@gmail.com>
2021-07-23 15:35:28 -0400

    added date times to queries

--- metrix-models/src/lib.rs
+++ metrix-models/src/lib.rs
@@ -1,8 +1,9 @@
-use serde::{Serialize, Deserialize};
 use uuid::Uuid;
+use chrono::prelude::*;
+use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Metric {
+pub struct MetricInsert {
     pub id: Uuid,
     pub data_point: String,
     pub data_group: Option<String>,
@@ -10,5 +11,22 @@ pub struct Metric {
     pub data_value_numeric: f64,
 }
 
-impl Metric {
+impl MetricInsert {
 }
+
+pub struct MetricQuery {
+    pub data_point: String,
+    pub data_group: Option<String>,
+}
+
+pub struct MetricPointQuery {
+    metric_query: MetricQuery,
+    date: DateTime<Utc>,
+}
+
+pub struct MetricRangeQuery {
+    metric_query: MetricQuery,
+    start_date: DateTime<Utc>,
+    end_date: DateTime<Utc>
+}
+
