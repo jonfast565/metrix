a659ef48b58ee697209991fbf3f0d45b89f81e41
Jon Fast <jnfstdj656@gmail.com>
2021-07-26 14:33:52 -0400

    added series function

--- metrix-database/src/lib.rs
+++ metrix-database/src/lib.rs
@@ -1,35 +1,57 @@
 #[macro_use]
 extern crate diesel;
-
-use crate::diesel::RunQueryDsl;
+use self::diesel::prelude::*;
+use self::models::*;
 
 pub mod models;
 pub mod schema;
 
 use diesel::PgConnection;
 
-pub fn insert_metric(model: &models::MetricInsertDb, conn: &PgConnection) {
+pub fn insert_metric(model: &MetricInsertDb, conn: &PgConnection) {
     diesel::insert_into(schema::metric::table)
         .values(model)
         .execute(conn)
         .expect("Error inserting row into database");
 }
 
-pub fn insert_metrics(models: Vec<&models::MetricInsertDb>, conn: &PgConnection) {
+pub fn insert_metrics(models: Vec<&MetricInsertDb>, conn: &PgConnection) {
     diesel::insert_into(schema::metric::table)
         .values(models)
         .execute(conn)
         .expect("Error inserting rows into database");
 }
 
-pub fn get_metric(model: models::MetricQueryDb, conn: &PgConnection) {
+pub fn get_metric(model: MetricQueryDb, conn: &PgConnection) -> Vec<MetricResultDb> {
     use schema::metric::dsl::*;
+    let results = metric
+        .filter(data_point.eq(model.data_point))
+        .filter(data_grouping.eq(model.data_group))
+        .limit(1)
+        .load::<MetricResultDb>(conn)
+        .expect("Error loading metric");
+    results
 }
 
-pub fn get_metric_history(model: models::MetricPointQueryDb, conn: &PgConnection) {
+pub fn get_metric_history(model: MetricPointQueryDb, conn: &PgConnection) -> Vec<MetricResultDb> {
     use schema::metric::dsl::*;
+    let results = metric
+        .filter(data_point.eq(model.data_point))
+        .filter(data_grouping.eq(model.data_group))
+        .filter(created_datetime.gt(model.date))
+        .load::<MetricResultDb>(conn)
+        .expect("Error loading metric history");
+    results
 }
 
-pub fn get_metric_series_history(model: models::MetricRangeQueryDb, conn: &PgConnection) {
+pub fn get_metric_series_history(model: MetricRangeQueryDb, conn: &PgConnection) -> Vec<MetricResultDb> {
     use schema::metric::dsl::*;
+    let results = metric
+        .filter(data_point.eq(model.data_point))
+        .filter(data_grouping.eq(model.data_group))
+        .filter(created_datetime.gt(model.start_date))
+        .filter(created_datetime.lt(model.end_date))
+        .load::<MetricResultDb>(conn)
+        .expect("Error loading metric series history");
+    results
 }
--- metrix-database/src/models.rs
+++ metrix-database/src/models.rs
@@ -1,9 +1,9 @@
-use diesel::pg::data_types::PgNumeric;
+use diesel::pg::data_types::{PgNumeric, PgTimestamp};
 use chrono::prelude::*;
 use uuid::Uuid;
 use crate::schema::metric;
 
-#[derive(Debug, Insertable)]
+#[derive(Debug, Insertable, Queryable)]
 #[table_name = "metric"]
 pub struct MetricInsertDb {
     pub id: Uuid,
@@ -32,4 +32,17 @@ pub struct MetricRangeQueryDb {
     pub data_group: Option<String>,
     pub start_date: NaiveDateTime,
     pub end_date: NaiveDateTime,
+}
+
+#[derive(Debug, Queryable)]
+pub struct MetricResultDb {
+    pub id: Uuid,
+    pub data_point: String,
+    pub data_type: String,
+    pub data_value_numeric: PgNumeric,
+    pub created_by: String,
+    pub created_datetime: PgTimestamp,
+    pub last_modified_by: String,
+    pub last_modified_by_datetime: PgTimestamp,
+    pub data_grouping: Option<String>,
 }
