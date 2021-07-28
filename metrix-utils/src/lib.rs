29e84c4c092641bb639d4033a4b3c5073ac1eebb
Jon Fast <jnfstdj656@gmail.com>
2021-07-28 17:42:19 -0400

    authors

--- metrix-utils/src/lib.rs
+++ metrix-utils/src/lib.rs
@@ -1,4 +1,5 @@
 use chrono::NaiveDateTime;
+use std::env;
 
 pub fn parse_iso_date_string(s: String) -> NaiveDateTime {
     NaiveDateTime::parse_from_str(&s, "%Y-%m-%dT%H:%M:%S.fZ").unwrap()
@@ -9,17 +10,22 @@ pub fn parse_iso_date_str(s: &str) -> NaiveDateTime {
 }
 
 pub fn get_header(application_name: &str) -> String {
+    let version = env!("CARGO_PKG_VERSION");
+    let authors = env!("CARGO_PKG_AUTHORS");
     format!(
-        r#"
+r#"
     __  ___     __       _     
-    /  |/  /__  / /______(_)  __
-   / /|_/ / _ \/ __/ ___/ / |/_/
-  / /  / /  __/ /_/ /  / />  <  
- /_/  /_/\___/\__/_/  /_/_/|_|  
- --------------------------------
- Application: {}
-
+   /  |/  /__  / /______(_)  __
+  / /|_/ / _ \/ __/ ___/ / |/_/
+ / /  / /  __/ /_/ /  / />  <  
+/_/  /_/\___/\__/_/  /_/_/|_|  
+--------------------------------
+Application: {}
+Author(s): {}
+Version: {}
 "#,
-        application_name
+        application_name,
+        authors,
+        version
     )
 }
