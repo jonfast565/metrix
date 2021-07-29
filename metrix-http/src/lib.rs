a12f0834a57304c5cfbbf3de2560ec1353874a7f
Jon Fast <jnfstdj656@gmail.com>
2021-07-29 16:04:01 -0400

    add http library

--- metrix-http/src/lib.rs
+++ metrix-http/src/lib.rs
@@ -1,26 +1,60 @@
 
 //#[post("/metric", format = "json", data = "<metric>")]
 pub async fn post_metric(model: metrix_models::MetricInsertPartial) {
-    
+    let client = reqwest::Client::new();
+    let _res = client.post("http://localhost:8000/metric").json(&model).send().await;
 }
 
 //#[get("/metric?<data_point>&<data_group>")]
 pub async fn get_metric(
     query: metrix_models::MetricQuery
 ) -> Vec<metrix_models::MetricResult> {
-    Vec::<metrix_models::MetricResult>::new()
+    let client = reqwest::Client::new();
+    let params = vec![
+        ("data_point", query.data_point),
+        ("data_group", query.data_group.unwrap())
+    ];
+    let res = client.get("http://localhost:8000/metric").query(&params).send().await;
+    let result = match res {
+        Ok(x) => x.json::<Vec<metrix_models::MetricResult>>().await.unwrap(),
+        Err(_) => panic!("Unable to get metric")
+    };
+    result
 }
 
 // #[get("/metric-history?<data_point>&<data_group>&<date>")]
 pub async fn get_metric_history(
     query: metrix_models::MetricPointQuery
 ) -> Vec<metrix_models::MetricResult> {
-    Vec::<metrix_models::MetricResult>::new()
+    let client = reqwest::Client::new();
+    let params = vec![
+        ("data_point", query.metric_query.data_point),
+        ("data_group", query.metric_query.data_group.unwrap()),
+        ("date", query.date.format(metrix_utils::time::FORMAT_STRING).to_string())
+    ];
+    let res = client.get("http://localhost:8000/metric-history").query(&params).send().await;
+    let result = match res {
+        Ok(x) => x.json::<Vec<metrix_models::MetricResult>>().await.unwrap(),
+        Err(_) => panic!("Unable to get metric")
+    };
+    result
 }
 
 // #[get("/metric-series?<data_point>&<data_group>&<start_date>&<end_date>")]
 pub async fn get_metric_series(
     query: metrix_models::MetricRangeQuery
 ) -> Vec<metrix_models::MetricResult> {
-    Vec::<metrix_models::MetricResult>::new()
+    let client = reqwest::Client::new();
+    let params = vec![
+        ("data_point", query.metric_query.data_point),
+        ("data_group", query.metric_query.data_group.unwrap()),
+        ("start_date", query.start_date.format(metrix_utils::time::FORMAT_STRING).to_string()),
+        ("end_date", query.end_date.format(metrix_utils::time::FORMAT_STRING).to_string())
+    ];
+    let res = client.get("http://localhost:8000/metric-series").query(&params).send().await;
+    let result = match res {
+        Ok(x) => x.json::<Vec<metrix_models::MetricResult>>().await.unwrap(),
+        Err(_) => panic!("Unable to get metric")
+    };
+    result
 }
--- metrix-utils/src/time.rs
+++ metrix-utils/src/time.rs
@@ -1,14 +1,16 @@
 use chrono::NaiveDateTime;
 
+pub const FORMAT_STRING : &'static str = "%Y-%m-%dT%H:%M:%S";
+
 pub fn parse_iso_date_string(s: String) -> NaiveDateTime {
-    match NaiveDateTime::parse_from_str(&s, "%Y-%m-%dT%H:%M:%S") {
+    match NaiveDateTime::parse_from_str(&s, FORMAT_STRING) {
         Ok(x) => x,
         Err(_) => panic!("Date {} is invalid. It should be in the format Y-m-dTH:M:S", s)
     }
 }
 
 pub fn parse_iso_date_str(s: &str) -> NaiveDateTime {
-    match NaiveDateTime::parse_from_str(s, "%Y-%m-%dT%H:%M:%S") {
+    match NaiveDateTime::parse_from_str(s, FORMAT_STRING) {
         Ok(x) => x,
         Err(_) => panic!("Date {} is invalid. It should be in the format Y-m-dTH:M:S", s)
     }
