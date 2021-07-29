16659db001fa1c0e36acb2d87d1afd236aef7bbd
Jon Fast <jnfstdj656@gmail.com>
2021-07-29 14:08:55 -0400

    use query params better.

--- metrix-ingest/src/request_models.rs
+++ metrix-ingest/src/request_models.rs
@@ -23,57 +23,50 @@ impl MetricInsertRequest<'_> {
     }
 }
 
-#[derive(FromForm)]
-pub struct MetricQueryRequest<'r> {
-    data_point: Option<&'r str>,
-    data_group: Option<&'r str>,
-}
-
-impl MetricQueryRequest<'_> {
-    pub fn to_metric_query(&self) -> metrix_models::MetricQuery {
-        metrix_models::MetricQuery {
-            data_point: self.data_point.unwrap().to_string(),
-            data_group: Some(self.data_group.unwrap().to_string())
+pub fn get_metric_model(
+    data_point: &str,
+    data_group: &str
+) -> metrix_models::MetricQuery {
+    metrix_models::MetricQuery {
+        data_point: data_point.to_string(),
+        data_group: match data_group {
+            "" => None,
+            s => Some(s.to_string())
         }
     }
 }
 
-#[derive(FromForm)]
-pub struct MetricPointQueryRequest<'r> {
-    data_point: Option<&'r str>,
-    data_group: Option<&'r str>,
-    date: Option<&'r str>,
-}
-
-impl MetricPointQueryRequest<'_> {
-    pub fn to_metric_point_query(&self) -> metrix_models::MetricPointQuery {
-        metrix_models::MetricPointQuery {
-            metric_query: metrix_models::MetricQuery {
-                data_point: self.data_point.unwrap().to_string(),
-                data_group: Some(self.data_group.unwrap().to_string())
+pub fn get_metric_history_model(
+    data_point: &str,
+    data_group: &str,
+    date: &str
+) -> metrix_models::MetricPointQuery {
+    metrix_models::MetricPointQuery {
+        metric_query: metrix_models::MetricQuery {
+            data_point: data_point.to_string(),
+            data_group: match data_group {
+                "" => None,
+                s => Some(s.to_string()),
             },
-            date: metrix_utils::time::parse_iso_date_str(self.date.unwrap())
-        }
+        },
+        date: metrix_utils::time::parse_iso_date_str(date),
     }
 }
 
-#[derive(FromForm)]
-pub struct MetricRangeRequest<'r> {
-    data_point: Option<&'r str>,
-    data_group: Option<&'r str>,
-    start_date: Option<&'r str>,
-    end_date: Option<&'r str>,
-}
-
-impl MetricRangeRequest<'_> {
-    pub fn to_metric_point_query(&self) -> metrix_models::MetricRangeQuery {
+pub fn get_metric_series_model( 
+    data_point: &str,
+    data_group: &str,
+    start_date: &str,
+    end_date: &str) -> metrix_models::MetricRangeQuery {
         metrix_models::MetricRangeQuery {
             metric_query: metrix_models::MetricQuery {
-                data_point: self.data_point.unwrap().to_string(),
-                data_group: Some(self.data_group.unwrap().to_string())
+                data_point: data_point.to_string(),
+                data_group: match data_group {
+                    "" => None,
+                    s => Some(s.to_string()),
+                },
             },
-            start_date: metrix_utils::time::parse_iso_date_str(self.start_date.unwrap()),
-            end_date: metrix_utils::time::parse_iso_date_str(self.end_date.unwrap())
+            start_date: metrix_utils::time::parse_iso_date_str(start_date),
+            end_date: metrix_utils::time::parse_iso_date_str(end_date),
         }
     }
-}
--- metrix-ingest/src/routes.rs
+++ metrix-ingest/src/routes.rs
@@ -1,8 +1,6 @@
-use crate::request_models::{
-    MetricInsertRequest, MetricPointQueryRequest, MetricQueryRequest, MetricRangeRequest,
-};
+use crate::request_models::MetricInsertRequest;
 use crate::MetrixDatabaseConnection;
-use rocket::serde::json::{Json};
+use rocket::serde::json::Json;
 
 #[post("/metric", format = "json", data = "<metric>")]
 pub async fn post_metric(conn: MetrixDatabaseConnection, metric: Json<MetricInsertRequest<'_>>) {
@@ -11,36 +9,42 @@ pub async fn post_metric(conn: MetrixDatabaseConnection, metric: Json<MetricInse
         .await;
 }
 
-#[get("/metric?<opt..>")]
+#[get("/metric?<data_point>&<data_group>")]
 pub async fn get_metric(
     conn: MetrixDatabaseConnection,
-    opt: MetricQueryRequest<'_>,
+    data_point: &str,
+    data_group: &str,
 ) -> Json<Vec<metrix_models::MetricResult>> {
-    let getter = opt.to_metric_query();
+    let getter = crate::request_models::get_metric_model(data_point, data_group);
     let result = conn
         .run(move |c| metrix_database::get_metrics(&getter, c))
         .await;
     Json(result)
 }
 
-#[get("/metric-point?<opt..>")]
+#[get("/metric-point?<data_point>&<data_group>&<date>")]
 pub async fn get_metric_history(
     conn: MetrixDatabaseConnection,
-    opt: MetricPointQueryRequest<'_>,
+    data_point: &str,
+    data_group: &str,
+    date: &str,
 ) -> Json<Vec<metrix_models::MetricResult>> {
-    let getter = opt.to_metric_point_query();
+    let getter = crate::request_models::get_metric_history_model(data_point, data_group, date);
     let result = conn
         .run(move |c| metrix_database::get_metric_history(&getter, c))
         .await;
     Json(result)
 }
 
-#[get("/metric-series?<opt..>")]
+#[get("/metric-series?<data_point>&<data_group>&<start_date>&<end_date>")]
 pub async fn get_metric_series(
     conn: MetrixDatabaseConnection,
-    opt: MetricRangeRequest<'_>,
+    data_point: &str,
+    data_group: &str,
+    start_date: &str,
+    end_date: &str,
 ) -> Json<Vec<metrix_models::MetricResult>> {
-    let getter = opt.to_metric_point_query();
+    let getter = crate::request_models::get_metric_series_model(data_point, data_group, start_date, end_date);
     let result = conn
         .run(move |c| metrix_database::get_metric_series_history(&getter, c))
         .await;
